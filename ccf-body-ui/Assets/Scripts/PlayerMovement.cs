8d7d7610bab08be3d536054ae97c06e36a33227d
Josh <josh.church.josh@gmail.com>
2022-06-14 21:05:14 -0400

    Script for player box movement completed

---
+++ ccf-body-ui/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementDistance = 0.001f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currPosition = transform.position;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(currPosition.x + movementDistance, currPosition.y, currPosition.z);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(currPosition.x - movementDistance, currPosition.y, currPosition.z);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(currPosition.x, currPosition.y + movementDistance, currPosition.z);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(currPosition.x, currPosition.y - movementDistance, currPosition.z);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.position = new Vector3(currPosition.x, currPosition.y, currPosition.z + movementDistance);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = new Vector3(currPosition.x, currPosition.y, currPosition.z - movementDistance);
+        }
+    }
+}
